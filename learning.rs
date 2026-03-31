9cb921b62a7e31379a0b98c1ccdc7c303f6432f2
Betty <betty.lossless@gmail.com>
2026-03-31 20:55:21 +0200

    quiz3 done yipee

--- learning.rs
+++ learning.rs
@@ -3,17 +3,44 @@ enum Idea {
     Number(i32)
 }
 
+
 impl Idea {
     fn answer(&mut self){
         match self {
-            Number => Number += 42,
+            Number => *Number += 42,
         }
     }
 }
+
+struct Plane {
+    Engine: String,
+    Wings: String,
+    Fuselage: String,
+    Passengers: u8,
+}
+
+//
+impl Plane {
+    fn fly(self){
+        println!("I'm flying!")
+    }
+}
+
 
 fn main(){
     let mut x:Idea = Idea::Number(5); 
     x.answer();
     dbg!(&x);
-    println!("{:?}",&x)
+    println!("{:?}",&x);
+    
+    let myPlane: Plane = Plane{
+                Engine:String::from("big one"), 
+                Wings:String::from("cool Biplane ones"), 
+                Fuselage:String::from("Gaping"),
+                Passengers: 5};
+    
+    myPlane.fly();
 }
+
+
+
